3905e1ee4c00011fdf19c6ae99acbacd82b7b934
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2025-02-18 20:44:30 -0600

    Completed imports on App.js and styled the background and Dashboard Header.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import LoginButton from './Components/LoginButton'
+import UserForm from './Components/UserForm'
+import UserList from './Components/UserList'
 
 function App() {
   return (
-    <div>
-      
+    <div className='max-w-2xl mx-auto p-6 bg-gray-100 min-h-screen'>
+      <h1 className='text-3xl font-bold text-center mb-4'>DASHBOARD</h1>
+      <LoginButton />
+      <UserForm />
+      <UserList />
     </div>
   )
 }
