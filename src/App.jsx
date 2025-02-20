0944d9c0bc116091910271f21af5a0114ffa3d2e
Isaiah Johnson <johnsonisaiah13@yahoo.com>
2025-02-20 10:55:52 -0600

    Project now adds users tot he list, styling has been applied, and is functioning properly.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import LoginButton from './Components/LoginButton'
 import UserForm from './Components/UserForm'
 import UserList from './Components/UserList'
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  const addUser = (newUser) => {
+    setUsers((prevUsers) => [...prevUsers, newUser])
+  }
+
   return (
     <div className='max-w-2xl mx-auto p-6 bg-gray-100 min-h-screen'>
       <h1 className='text-3xl font-bold text-center mb-4'>DASHBOARD</h1>
       <LoginButton />
-      <UserForm />
-      <UserList />
+      <UserForm onAddUser={addUser} />
+      <UserList users={users} />
     </div>
   )
 }
--- src/Components/UserList.jsx
+++ src/Components/UserList.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
-function UserList() {
+function UserList({ users }) {
   return (
-    <div>
-      
+    <div className='bg-white p-4 rounded-lg shadow-md max-w-md mx-auto'>
+      <h2 className='text-xl font-semibold mb-4 text-center'>Users List</h2>
+      {users.length === 0 ? (
+        <p className='text-gray-600 text-center'>NO USERS</p>
+      ) : (
+        <ul className='space-y-4'>
+            {users.map((user) => (
+                <li key={user.id} className='border p-3 rounded hover:bg-gray-100'>
+                    <p><strong>Name:</strong> {user.name}</p>
+                    <p><strong>Age:</strong> {user.age}</p>
+                    <p><strong>City:</strong> {user.city}</p>
+                </li>
+            ))}
+        </ul>
+      )}
     </div>
   )
 }
